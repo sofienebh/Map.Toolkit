85840a37d73a9558bc689d13bbc6c6be9e8b3092
sbenhamouda <sofienebenhamouda@gmail.com>
2019-03-11 22:17:19 +0100

    Second Test

--- MapToolkit/Map.Toolkit.Test/ComputeContinentTest.cs
+++ MapToolkit/Map.Toolkit.Test/ComputeContinentTest.cs
@@ -18,5 +18,16 @@ namespace Map.Toolkit.Test
             var expectedContinentCount = 0;
             Check.That(continentNumber).IsEqualTo(expectedContinentCount);
         }
+
+        [Test]
+        public void The_number_of_continents_should_be_0_when_map_is_00()
+        {
+            int[][] mapStructure = { new[] { 0, 0 } };
+            _map = new MapToolkit.Model.Map(mapStructure);
+            var continentNumber = _map.ComputeContinent();
+
+            var expectedContinentCount = 0;
+            Check.That(continentNumber).IsEqualTo(expectedContinentCount);
+        }
     }
 }
